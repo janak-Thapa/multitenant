669ae4abb8eb131c30f45c9f295de6bdb3d85507
janak <ogjanak1@gmail.com>
2025-05-22 08:21:51 +0545

    customize and try neobrutalism theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 
 
 export default function Home() {
   return (
-    <div>
-      <p className="text-rose-500 text-3xl">Hello world</p>
+    <div className="flex flex-col gap-y-4">
+      <div>
+      <Button variant={'elevated'}>Button</Button>
+      </div>
+      <div>
+        <Input placeholder="Enter your email" />
+      </div>
+      <div>
+        <Progress value={50}/>
+      </div>
+      <div>
+        <Textarea   placeholder="Enter your message"/>
+      </div>
+      <div>
+        <Checkbox/>
+      </div>
     </div>
   );
 }
